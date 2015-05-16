08b22c6b0715f032aba193ecc9939e9cbc5c7673
denisx <denisx@denisx.ru>
2015-05-16 17:13:13 +0300

    add

--- run.sh
+++ run.sh
@@ -3,11 +3,15 @@
 cd ~/Dropbox/private/own/journal-source/
 jekyll build --destination ../journal-render
 git pull
+git checkout master
+git pull
 git add .
 git commit -m "add"
 git push -u origin master
 
 cd ../journal-render
+git pull
+git checkout gh-pages
 git pull
 git add .
 git commit -m "add"
